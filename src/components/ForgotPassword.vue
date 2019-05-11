a8b9f02f1c70b8104eefc0902fe27de6d18addf9
noriki1971997 <phi.nguyenviet197@gmail.com>
2019-05-11 08:05:51 +0700

    - change ui change pass

--- src/components/ForgotPassword.vue
+++ src/components/ForgotPassword.vue
@@ -7,19 +7,27 @@
 
             <div class="form-group" >
                 <label class="lb-form-group">Mật khẩu mới</label>
-                <input type="text" v-model="password.new" class="input-forgotpass" />
+                <input :class="{'is-danger': errors.has('password')}" v-validate="'required'" name="password" type="password" v-model="password.new" class="input-forgotpass" ref="password" />
             </div>
             <div class="form-group" >
-                <label class="lb-form-group msgNotify">{{result}}</label>
+                <label v-show="errors.has('password')" class="lb-form-group msgNotify">{{ errors.first('password') }}</label>
             </div>
             <div class="form-group" >
                 <label class="lb-form-group">Xác nhận mật khẩu</label>
-                <input type="text" v-model="password.confirm" class="input-forgotpass"/>
+                <input :class="{'is-danger': errors.has('password_confirmation')}" v-validate="'required|confirmed:password'" name="password_confirmation" type="password" v-model="password.confirm" class="input-forgotpass"/>
+            </div>
+             <div class="form-group" >
+                <label v-show="errors.has('password_confirmation')" class="lb-form-group msgNotify">{{ errors.first('password_confirmation') }}</label>
             </div>
             <div class="form-group-btn" >
                 <button class="btn-primary">Gữi</button>
             </div>
         </form>
+        <div class="result-zone">
+          <label class="result-label">
+            {{result}}
+          </label>
+        </div>
       </div>
 
   
@@ -36,16 +44,16 @@
   position: relative;
   margin-left: auto;
   margin-right: auto;
-  margin-top: 10em;
+  margin-top: 5em;
   marker-bottom:auto;
   font-size: 16px;
-  width:25em;
-  height:23em;
+  width:28em;
+  height:28em;
   align-items: center;
 }
 .form-content
 {
-  padding-top: 3em;
+  padding-top: 2.5em;
   padding-bottom: 2em;
   position: relative;
   width: 60%;
@@ -60,13 +68,14 @@
   margin-left: auto;
   margin-right: auto;
   width: 100%;
-  padding-top: 1em;
+  padding-top: 1.3em;
+  height: 2.5em;
 }
 .input-forgotpass
 {
   width: 100%;
   height: 2em;
-  border: 2px solid #41b6e6;
+  border: 2px solid #55efc4;
   border-radius: 0.3em;
   background-color: #dbebfa;
   padding-left: .5em;
@@ -81,10 +90,9 @@
 .lb-title-form-group
 {
   text-align: center;
-  font-size: 20px;
+  font-size: 26px;
   font-weight: 700;
   color:#f9f8eb;
-  padding-bottom: 1em;
 }
 .form-group-btn
 {
@@ -97,7 +105,7 @@
 {
 
   position: relative;
-  margin-top: 2em;
+  margin-top: 1em;
   margin-right: auto;
   margin-left: auto;
   width: 5em;
@@ -110,7 +118,22 @@
 }
 .msgNotify
 {
+  font-size: 16px;
   color:#F50057;
+  width: 100%;
+  text-align: left;
+}
+.result-zone
+{
+  position: relative;
+  font-size: 18px;
+  color:white;
+  margin-top: 7.5em;
+  margin-right: auto;
+  margin-left: auto;
+  width: 100%;
+  text-align: center;
+  color: #F50057;
 }
 </style>
 
@@ -140,15 +163,20 @@ export default {
   methods:
   {
    handleSubmit(e) {
-
-      axios.post("http://13.67.50.208:3003/api/auth/forgot-pass/",{token:this.token,password: this.password.new,c_password:this.password.confirm})
+      this.result="";
+      this.$validator.validate().then(valid => {
+        if (valid) {
+          axios.post("http://13.67.50.208:3003/api/auth/forgot-pass/",{token:this.token,password: this.password.new,c_password:this.password.confirm})
               .then(resp => {
                 console.log(resp);
                 this.result = resp.data.message;
               })
               .catch(err => {
-                 this.result = err;
+                 this.result = "Lỗi không thể đổi mật khẩu";
               })
+        }
+      });
+      
     }
   }
 };
